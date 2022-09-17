6e62695850c65b9139139c1a3ea72539b9feea6d
Cole Fortner <colefortner@gmail.com>
2022-09-16 23:06:33 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [stuff, setStuff] = useState();
+  useEffect(() => {
+    fetch("http://localhost:8080/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setStuff(data[0]);
+        // console.log(data);
+      });
+  }, []);
+  console.log(stuff);
+  return <div className="App">{stuff.name}</div>;
 }
 
 export default App;
